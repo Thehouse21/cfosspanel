52045341cd701e3709a8dd7c22cffa92e1c9306c
haco1971 <omsameh2010@gmail.com>
2017-10-12 14:11:54 +0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-apt-get upgrade -y --force-yes > /dev/null
+apt-get upgrade -y --force-yes 
 echo "Done"
 echo "Installing FOS Prerequisites..."
 apt-get install php5-cli php5-curl curl zip unzip sudo nano dialog apt-utils python-software-properties apt lsb-release -y > /dev/null
 echo "Done"
 
-chmod 777 /tmp > /dev/null
-rm -rf ~/bin  > /dev/null
-rm -rf ~/ffmpeg*  > /dev/null
-cd /tmp > /dev/null
-rm -rf /tmp/* > /dev/null
+chmod 777 /tmp 
+rm -rf ~/bin  
+rm -rf ~/ffmpeg*  
+cd /tmp 
+rm -rf /tmp/* 
 echo "FOS-Streaming -> Web Platform"
-wget -q https://raw.githubusercontent.com/vaniusha3/IPTV-MD/master/install_panel.php -O install_panel.php > /dev/null
+wget -q https://raw.githubusercontent.com/vaniusha3/IPTV-MD/master/install_panel.php -O install_panel.php 
 /usr/bin/php install_panel.php
 echo "FOS-Streaming -> Database Deployment"
-wget -q https://raw.githubusercontent.com/vaniusha3/IPTV-MD/master/db_install.sh -O db_install.sh > /dev/null 
-chmod 755 db_install.sh > /dev/null
+wget -q https://raw.githubusercontent.com/vaniusha3/IPTV-MD/master/db_install.sh -O db_install.sh  
+chmod 755 db_install.sh 
 ./db_install.sh
 if [ ! -f /usr/bin/ffmpeg ]; then
 echo "FOS-Streaming -> FFmpeg and FFprobe installation"
-wget -q https://raw.githubusercontent.com/vaniusha3/IPTV-MD/master/ffmpeg.sh -O ffmpeg.sh > /dev/null
-chmod 755 ffmpeg.sh > /dev/null
+wget -q https://raw.githubusercontent.com/vaniusha3/IPTV-MD/master/ffmpeg.sh -O ffmpeg.sh 
+chmod 755 ffmpeg.sh 
 ./ffmpeg.sh
 fi
